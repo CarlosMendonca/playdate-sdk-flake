4d6ff5eec7f40c1d37309789dad84280f561d81b
Carlos Mendonça <CarlosMendonca@users.noreply.github.com>
2025-12-27 00:31:54 -0500

    Simplifying to only allow installation on a local .PlaydateSDK directory, which avoids some PlaydateSimulator limitations

--- playdate-sandbox-setup.sh
+++ playdate-sandbox-setup.sh
@@ -2,41 +2,32 @@
 # Playdate SDK Sandbox Setup Script
 # This script ensures PlaydateSimulator has a writable environment
 
-# Determine sandbox location
-SANDBOX_DIR=""
-XDG_FALLBACK="${XDG_DATA_HOME:-$HOME/.local/share}/PlaydateSDK"
+PLAYDATE_SDK_DIR=".PlaydateSDK"
+SANDBOX_DIR="$(pwd)/$PLAYDATE_SDK_DIR"
 
-if [ -d ".PlaydateSDK" ]; then
-  SANDBOX_DIR="$(pwd)/.PlaydateSDK"
-else
-  # No local sandbox, ask user where they want it
+if [ ! -d "$SANDBOX_DIR" ]; then
   echo "PlaydateSimulator: No local .PlaydateSDK found."
-  echo "  [1] Create in current directory: $(pwd)/.PlaydateSDK"
-  echo "  [2] Use XDG data directory: $XDG_FALLBACK"
-  read -p "Choose [1/2] or cancel [n]: " -n 1 -r
+  read -p "Create .PlaydateSDK in $(pwd)? [y/n]: " -n 1 -r
   echo
 
-  if [[ $REPLY =~ ^[1]$ ]]; then
-    SANDBOX_DIR="$(pwd)/.PlaydateSDK"
-  elif [[ $REPLY =~ ^[2]$ ]]; then
-    SANDBOX_DIR="$XDG_FALLBACK"
-  else
+  if [[ ! $REPLY =~ ^[Yy]$ ]]; then
     echo "PlaydateSimulator: Cancelled"
     exit 1
   fi
 
-  # Create sandbox if it doesn't exist
-  if [ ! -d "$SANDBOX_DIR" ]; then
-    echo "PlaydateSimulator: Creating sandbox at $SANDBOX_DIR"
-    mkdir -p "$SANDBOX_DIR"
-    cp -TR @PLAYDATE_SDK@/Disk "$SANDBOX_DIR/Disk"
-    chmod -R 755 "$SANDBOX_DIR/Disk"
-    ln -s @PLAYDATE_SDK@/bin "$SANDBOX_DIR/bin"
-    ln -s @PLAYDATE_SDK@/C_API "$SANDBOX_DIR/C_API"
-    ln -s @PLAYDATE_SDK@/CoreLibs "$SANDBOX_DIR/CoreLibs"
-    ln -s @PLAYDATE_SDK@/Resources "$SANDBOX_DIR/Resources"
-  fi
+  echo "PlaydateSimulator: Creating sandbox at $SANDBOX_DIR"
+  mkdir -p "$SANDBOX_DIR"
+  cp -r @PLAYDATE_SDK@/Disk "$SANDBOX_DIR/Disk"
+  chmod -R 755 "$SANDBOX_DIR/Disk"
+  ln -s @PLAYDATE_SDK@/bin "$SANDBOX_DIR/bin"
+  ln -s @PLAYDATE_SDK@/C_API "$SANDBOX_DIR/C_API"
+  ln -s @PLAYDATE_SDK@/CoreLibs "$SANDBOX_DIR/CoreLibs"
+  ln -s @PLAYDATE_SDK@/Resources "$SANDBOX_DIR/Resources"
 fi
 
 echo "PlaydateSimulator: Using sandbox at $SANDBOX_DIR"
-export PLAYDATE_SDK_PATH="$SANDBOX_DIR"
+
+# Set the PLAYDATE_SDK_PATH
+# This seems redundant, but it's not guaranteed to be set
+# For whatever reason, PlaydateSimulator doesn't like absolute paths -- go figure
+export PLAYDATE_SDK_PATH="$PLAYDATE_SDK_DIR"
